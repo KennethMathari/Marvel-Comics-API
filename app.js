bac159d6bf2df870c8ec56883fccb6d58cc17716
KennethNdungu <mathari580@gmail.com>
2021-09-14 13:50:24 +0300

    Displayed Callback Response on UI

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // Marvel Characters API URL
 const api_url = "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=edeb36852f8696cb83c487b2279bf494&hash=4f143c76c354fbfeead772ab3f62a179";
 
+//DOM
+let message= document.getElementById('message');
+
 async function getapi(url) {
     
     // Storing response
@@ -14,10 +17,14 @@ async function getapi(url) {
             return response.json();
     }).then(function(data){
         // Logs the Marvel Characters API data
-            console.log('Marvel Characters:', data);
+            console.log('Marvel Universe Characters:', data);
+            //Display on UI
+            message.innerHTML='<p>Marvel Universe Characters</p>';
     }).catch(function (error) {
         // Logs any errors
         console.log('Oops! Something went wrong', error);
+        //Display on UI
+        message.innerHTML='Oops! Thanos snapped!';
       });
     
 }
