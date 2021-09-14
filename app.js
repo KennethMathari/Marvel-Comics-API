fbd2afb65e3830f2838c9e24af353ee8942a6f28
KennethNdungu <mathari580@gmail.com>
2021-09-14 14:47:41 +0300

    Updated Header UI for Error Response

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ async function getapi(url) {
         // Logs any errors
         console.log('Oops! Something went wrong', error);
         //Display on UI
-        message.innerHTML='Oops! Thanos snapped!';
+        header.innerHTML='<div role="alert" class="p-20"><div class="bg-red-500 text-white font-bold rounded-t px-4 py-2">Oops!</div><div class="border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700"><p>Thanos snapped.</p></div></div>';
       });
     
 }
