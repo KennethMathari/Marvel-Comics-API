11fe11e43b4521deebbdce75346c44f099e9c9b1
KennethNdungu <mathari580@gmail.com>
2021-09-29 12:22:15 +0300

    Update app.js

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ const APP={
       //check if Service Worker is Supported
       if('serviceWorker' in navigator){
         //Register Service Worker
-        navigator.serviceWorker.register(['/sw_cache_assets.js','/sw_cache_site.js'],{
+        navigator.serviceWorker.register('/sw_cache_assets.js',{
             scope:'/'
         })
         .then(registration =>{
