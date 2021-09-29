81e72c47e76612d9fd1bdc45c3ba392b9e9d2181
KennethNdungu <mathari580@gmail.com>
2021-09-29 16:13:42 +0300

    Stored URL in cache
    still unable to store json content

--- sw_cache_assets.js
+++ sw_cache_assets.js
@@ -2,26 +2,27 @@ const cacheName = 'marvelCachev1';
 const api_url = "https://gateway.marvel.com/v1/public/characters?ts=1&apikey=edeb36852f8696cb83c487b2279bf494&hash=4f143c76c354fbfeead772ab3f62a179";
 
 
-// const cacheAssets =[
-//     'app.js',
-//     'composer.json',
-//     'index.html',
-//     'index.php',
-//     'sw_cache_assets.js',
-//     'sw_cache_site.js'
-// ]
-
-
 self.addEventListener('install', (ev) => {
     //service worker is installed.
     console.log('Service worker installed');
 
     ev.waitUntil(
         fetch(api_url).then(res => {
-           return res.json();
+           
+          return res.json();
+
           })
           .then(function(data){
             console.log('Marvel Universe Characters:', data);
+            caches.open(cacheName).then(cache => {
+                cache.put(api_url, new Response(api_url)).then( () => {
+                    // console.log(new Response(api_url))
+                 });
+              })
+
+          })
+          .catch(function (error) {
+            console.log('Oops! Something went wrong', error);
           })
     );
   });
